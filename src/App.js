60a39841f1fc5056a299c30d718d47fbcc75f94c
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2022-11-03 00:44:16 -0400

    mapping and arrow function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
+import notes from "./notes";
+
+
 
 function App() {
   return (
     <div className="App">
      <Header />
-     <Note />
+     {notes.map((content) => <Note 
+    key = {content.key}
+    title = {content.title}
+    content = {content.content}
+  />)}
+  
      <Footer />
         
       
